62962b4c805fedc5a31cfd7294b07e118c2222d3
Ujjwal Pandey <ujjwalpandey922@gmail.com>
2023-10-03 22:35:45 +0530

    ACAO

--- Server/index.js
+++ Server/index.js
@@ -13,6 +13,7 @@ app.use(
 ); //cors
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  next();
 }); 
 app.use(express.json()); // parse data into json
 env.config();
